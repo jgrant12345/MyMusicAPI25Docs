37611f2260fbac93b890d89035aa24034dfcba19
unknown <justingrant99@me.com>
2021-09-15 23:18:42 -0700

    more

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -2,7 +2,8 @@ import { MyRoute } from "./RouteStyles"
 export function Routes(){
     return(
         <div>
-        <MyRoute>GET: https://mymusicapi25. herokuapp.com/v1/bands/&#123;bandName&#125; </MyRoute>
+        <MyRoute>GET: https://mymusicapi25. herokuapp.com/v1/bands/ </MyRoute>
+       <div>attributes</div>
         </div>
     )
 }
